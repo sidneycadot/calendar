eade6edf1cced12d2b6553fd2c7409189b14f454
Sidney Cadot <sidney@jigsaw.nl>
2022-03-12 14:55:14 +0100

    * Renamed __int__ method to x.to_julian_day_number() * Added comparison operators and explained their meaning. * factored tests

--- calendar.py
+++ calendar.py
@@ -2,6 +2,7 @@
 
 #pylint: disable=line-too-long
 
+
 def is_divisible_by(n: int, divider: int) -> bool:
     """Return True if the first argument is divisible by the second argument, and False if not."""
     return n % divider == 0
@@ -11,13 +12,14 @@ def normalize_year(year: int) -> int:
     """Normalize year for the lack of a year zero.
 
     In Julian and Gregorian calendar dates, year zero is never used; the year 1 BCE (-1) is
-    followed by the year 1 CE (+1). This discontinuity makes the calculations more cumbersome
-    than they should be.
+    followed directly by the year 1 CE (+1). This discontinuity makes the calculations more
+    cumbersome than they should be.
 
-    For that reason, we increase BCE years by 1, making the year sequence continuous.
+    For that reason, we increase BCE years by 1, making the year sequence continuous. We
+    call the resulting year indication a 'normalized year'.
     """
     if year == 0:
-        raise ValueError()
+        raise ValueError("Year zero does not exist, so cannot be normalized.")
 
     return year if year > 0 else year + 1
 
@@ -25,14 +27,14 @@ def normalize_year(year: int) -> int:
 class AbstractCalendarDate:
     """This is the base class of the JulianCalendarDate and GregorianCalendarDate classes.
 
-    It implements the common functionality between these two calendar classes.
+    It implements the common functionality shared by these two calendar classes.
 
     The AbstractCalendarDate should not be instantiated directly.
     """
 
     # pylint: disable=no-member
 
-    # Days before a month, for a year starting in March.
+    # Days before a month, for a 12-month year starting in March.
     # This table is used to convert between dates and Julian day numbers.
     days_before_month = (0, 31, 61, 92, 122, 153, 184, 214, 245, 275, 306, 337)
 
@@ -40,7 +42,7 @@ class AbstractCalendarDate:
         """Generic initialization of a calendar date (Gregorian or Julian)."""
         valid_date = (year != 0) and (1 <= month <= 12) and (1 <= day <= self.length_of_month(year, month))
         if not valid_date:
-            raise ValueError()
+            raise ValueError("Attempt to initialize an invalid date.")
 
         self.year  = year
         self.month = month
@@ -51,20 +53,52 @@ class AbstractCalendarDate:
 
         This makes it possible to convert a date to a tuple, for example.
         """
-        yield self.year
-        yield self.month
-        yield self.day
+        yield from (self.year, self.month, self.day)
 
     def __repr__(self) -> str:
         """String representation of a calendar date."""
         return "{}(year = {}, month = {}, day = {})".format(self.__class__.__name__, *self)
 
+    # Below, we provide implementations for the six comparison operators in Python.
+    #
+    # We do comparison based on the underlying days denoted by the calendar dates, and
+    # do not consider the difference between the types (JulkianCalendarDate, GregorianCalendarDate).
+    #
+    # Thus, a JulianCalendarDate may compare equal to a GregorianCalendarDate, e.g., the comparison
+    #
+    #   JulianCalendarDate(1752, 9, 2) == GregorianCalendarDate(1752, 9, 13)
+    #
+    # will evaluate as True.
+
+    def __lt__(self, rhs: 'AbstractCalendarDate') -> bool:
+        """Return True if the lhs date is strictly before the rhs date, False otherwise."""
+        return self.to_julian_day_number() < rhs.to_julian_day_number()
+
+    def __le__(self, rhs: 'AbstractCalendarDate') -> bool:
+        """Return True if the lhs date is before or on the rhs date, False otherwise."""
+        return self.to_julian_day_number() <= rhs.to_julian_day_number()
+
     def __eq__(self, rhs: 'AbstractCalendarDate') -> bool:
-        """Return True if the calendar date types and dates match, False otherwise."""
-        return (self.__class__ is rhs.__class__) and (tuple(self) == tuple(rhs))
+        """Return True if the lhs date is on the same day as the right hand side day, False otherwise."""
+        return self.to_julian_day_number() == rhs.to_julian_day_number()
+
+    def __ne__(self, rhs: 'AbstractCalendarDate') -> bool:
+        """Return True if the lhs date is not on the same day as the right hand side day, False otherwise."""
+        return self.to_julian_day_number() != rhs.to_julian_day_number()
+
+    def __gt__(self, rhs: 'AbstractCalendarDate') -> bool:
+        """Return True if the lhs date strictly after the right hand side day, False otherwise."""
+        return self.to_julian_day_number() > rhs.to_julian_day_number()
+
+    def __ge__(self, rhs: 'AbstractCalendarDate') -> bool:
+        """Return True if the lhs date is on or after the right hand side day, False otherwise."""
+        return self.to_julian_day_number() >= rhs.to_julian_day_number()
 
     def __next__(self) -> 'AbstractCalendarDate':
-        """Calculate the next calendar day."""
+        """Calculate the next calendar day.
+        
+        Note that this method skips over the year zero.
+        """
         (year, month, day) = self
 
         if day == self.length_of_month(year, month):
@@ -91,22 +125,22 @@ class AbstractCalendarDate:
 
         valid_month = (year != 0) and (1 <= month <= 12)
         if not valid_month:
-            raise ValueError()
+            raise ValueError("Invalid month specified.")
 
         # The months January, March, May, July, August, October, and December have 31 days.
         # The months April, June, September, and November have 30 days.
         # The month February has 28 days in a regular year and 29 days in a leapyear.
-        return (29 if cls.is_leapyear(year) else 28) if month == 2 else 30 if month in (4, 6, 9, 11) else 31
+        return (29 if cls.is_leapyear(year) else 28) if month == 2 else (30 if month in (4, 6, 9, 11) else 31)
 
-    def __int__(self) -> int:
+    def to_julian_day_number(self) -> int:
         """Convert a calendar date to a Julian day number."""
 
         (year, month, day) = self
 
         normalized_year = normalize_year(year)
 
-        # Start the year in March, moving February (and its leap day, if present)
-        # to the end of the year.
+        # Start the year in March, moving January and February (and its leap day, if present)
+        # to the end of the previous year.
         month -= 3
         if month < 0:
             month += 12
@@ -115,12 +149,17 @@ class AbstractCalendarDate:
         day -= 1
 
         # Calculate and return the Julian day number.
-        return self.__class__._year_julian_day_number(normalized_year) + AbstractCalendarDate.days_before_month[month] + day
+        return self._year_julian_day_number(normalized_year) + AbstractCalendarDate.days_before_month[month] + day
 
     @classmethod
     def from_julian_day_number(cls, julian_day_number: int) -> 'AbstractCalendarDate':
         """Convert a Julian day number to a calendar date."""
 
+        # We perform our calculations with years that start in March. The advantage of this is that
+        # February will be the last month in the year, and its leapday (if applicable) will be the last month
+        # of the year; this simplifies the calculations that follow.
+
+        # "Day zero" for our calculations will be March 1st in the year 1 BCE, which is our 'year zero'.
         year = 0
         julian_day_number -= cls._year_julian_day_number(0)
 
@@ -147,7 +186,7 @@ class AbstractCalendarDate:
             month -= 12
             year += 1
 
-        # Skip over year 0.
+        # Skip over year 0 ("un-normalize" the year).
 
         if year <= 0:
             year -= 1
--- test_calendar.py
+++ test_calendar.py
@@ -12,29 +12,43 @@ from calendar import JulianCalendarDate, GregorianCalendarDate
 class TestJulianCalendarDate(unittest.TestCase):
     """Test the Julian calendar date functionality."""
 
-    def test_conversion_to_and_from_julian_day_number(self):
-        """Test conversion of a Julian calendar date to a Julian day number and vice versa."""
+    def test_conversion_to_julian_day_number(self):
+        """Test conversion of a Julian calendar date to a Julian day number."""
 
         # Julian day 0 is January 1st, 4713 BCE in the proleptic Julian calendar.
         julian_date = JulianCalendarDate(-4713,  1,  1)
         julian_day_number = 0
 
-        # Iterate over days up to the year 5000.
-        while julian_date.year < 5000:
+        # Iterate over dates up to and including the year 5000.
+        while julian_date.year <= 5000:
 
-            julian_day_number_from_julian_date = int(julian_date)
-
+            julian_day_number_from_julian_date = julian_date.to_julian_day_number()
             self.assertEqual(julian_day_number_from_julian_date, julian_day_number)
+
+            julian_date = next(julian_date)
+            julian_day_number += 1
+
+    def test_conversion_from_julian_day_number(self):
+        """Test conversion of a Julian day number to a Julian calendar date."""
+
+        # Julian day 0 is January 1st, 4713 BCE in the proleptic Julian calendar.
+        julian_date = JulianCalendarDate(-4713,  1,  1)
+        julian_day_number = 0
+
+        # Iterate over dates up to and including the year 5000.
+        while julian_date.year <= 5000:
 
             julian_date_from_julian_day_number = JulianCalendarDate.from_julian_day_number(julian_day_number)
 
-            self.assertEqual(julian_date, julian_date_from_julian_day_number)
+            # Note that we compare the (Y, M, D) tuples here rather than comparing the dates directly.
+            # We do this to do prevent a date-compare that depends on the conversion-to-julian-day-number.
+            self.assertEqual(tuple(julian_date), tuple(julian_date_from_julian_day_number))
 
             julian_date = next(julian_date)
             julian_day_number += 1
 
     def test_year_julian_day_number(self):
-        """Verify that the '_year_julian_day_number' method yields the Julian day number of March 1st.
+        """Verify that the internal '_year_julian_day_number' method yields the Julian day number of March 1st.
 
         Note: this test presumes that the calendar methods are correct.
         """
@@ -46,7 +60,7 @@ class TestJulianCalendarDate(unittest.TestCase):
             normalized_year = year if year > 0 else year + 1
 
             julian_day_number_1 = JulianCalendarDate._year_julian_day_number(normalized_year)
-            julian_day_number_2 = int(JulianCalendarDate(year, 3, 1))
+            julian_day_number_2 = JulianCalendarDate(year, 3, 1).to_julian_day_number()
 
             self.assertEqual(julian_day_number_1, julian_day_number_2)
 
@@ -54,23 +68,36 @@ class TestJulianCalendarDate(unittest.TestCase):
 class TestGregorianCalendarDate(unittest.TestCase):
     """Test the Gregorian calendar date functionality."""
 
-    def test_conversion_to_and_from_julian_day_number(self):
-        """Test conversion of a Gregorian calendar date to a Julian day number and vice versa."""
+    def test_conversion_to_julian_day_number(self):
+        """Test conversion of a Gregorian calendar date to a Julian day number."""
 
         # Julian day 0 is November 24th, 4714 BCE in the proleptic Gregorian calendar.
         gregorian_date = GregorianCalendarDate(-4714,  11,  24)
         julian_day_number = 0
 
-        # Iterate over days up to the year 5000.
-        while gregorian_date.year <= 9999:
+        # Iterate over dates up to and including the year 5000.
+        while gregorian_date.year <= 5000:
 
-            julian_day_numberfrom_gregorian_date = int(gregorian_date)
+            julian_day_number_from_gregorian_date = gregorian_date.to_julian_day_number()
+            self.assertEqual(julian_day_number_from_gregorian_date, julian_day_number)
 
-            self.assertEqual(julian_day_numberfrom_gregorian_date, julian_day_number)
+            gregorian_date = next(gregorian_date)
+            julian_day_number += 1
 
+    def test_conversion_from_julian_day_number(self):
+        """Test conversion of a Julian day number to a Gregorian calendar date."""
+
+        # Julian day 0 is November 24th, 4714 BCE in the proleptic Gregorian calendar.
+        gregorian_date = GregorianCalendarDate(-4714,  11,  24)
+        julian_day_number = 0
+
+        # Iterate over dates up to and including the year 5000.
+        while gregorian_date.year <= 5000:
+
+            # Note that we compare the (Y, M, D) tuples here rather than comparing the dates directly.
+            # We do this to do prevent a date-compare that depends on the conversion-to-julian-day-number.
             gregorian_date_from_julian_day_number = GregorianCalendarDate.from_julian_day_number(julian_day_number)
-
-            self.assertEqual(gregorian_date, gregorian_date_from_julian_day_number)
+            self.assertEqual(tuple(gregorian_date), tuple(gregorian_date_from_julian_day_number))
 
             gregorian_date = next(gregorian_date)
             julian_day_number += 1
@@ -88,9 +115,35 @@ class TestGregorianCalendarDate(unittest.TestCase):
             normalized_year = year if year > 0 else year + 1
 
             julian_day_number_1 = GregorianCalendarDate._year_julian_day_number(normalized_year)
-            julian_day_number_2 = int(GregorianCalendarDate(year, 3, 1))
+            julian_day_number_2 = GregorianCalendarDate(year, 3, 1).to_julian_day_number()
 
             self.assertEqual(julian_day_number_1, julian_day_number_2)
+
+
+class TestAbstractCalendarDates(unittest.TestCase):
+
+    def test_calendar_datecomparisons(self):
+        """Test the six comparison operators for Julian and Gregorian dates."""
+
+        test_dates = (
+            JulianCalendarDate   (1752, 9,  2),
+            GregorianCalendarDate(1752, 9, 13),
+            JulianCalendarDate   (1752, 9, 13),
+            GregorianCalendarDate(1752, 9, 24)
+        )
+
+        # All comparisons work by comparing the julian day numbers of dates,
+        # meaning dates of different types (JulianCalendarDate, GregorianCalendarDate)
+        # can compare as equal.
+
+        for date_1 in test_dates:
+            for date_2 in test_dates:
+                self.assertEqual(date_1 <  date_2, date_1.to_julian_day_number() <  date_2.to_julian_day_number())
+                self.assertEqual(date_1 <= date_2, date_1.to_julian_day_number() <= date_2.to_julian_day_number())
+                self.assertEqual(date_1 == date_2, date_1.to_julian_day_number() == date_2.to_julian_day_number())
+                self.assertEqual(date_1 != date_2, date_1.to_julian_day_number() != date_2.to_julian_day_number())
+                self.assertEqual(date_1 >  date_2, date_1.to_julian_day_number() >  date_2.to_julian_day_number())
+                self.assertEqual(date_1 >= date_2, date_1.to_julian_day_number() >= date_2.to_julian_day_number())
 
 
 if __name__ == "__main__":
